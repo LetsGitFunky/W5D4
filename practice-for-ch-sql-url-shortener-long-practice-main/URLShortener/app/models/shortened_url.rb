107f236495182298c0ab304c7931067304be7cbc
laurencary <armstronglg720@gmail.com>
2023-04-27 17:29:17 -0700

    started random_code

--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
@@ -9,6 +9,8 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+require 'securerandom'
+
 class ShortenedUrl < ApplicationRecord
     validates :long_url, presence: true, uniqueness: true
     # long_url, presence: true, uniqueness: true
@@ -18,4 +20,10 @@ class ShortenedUrl < ApplicationRecord
         foreign_key: :user_id,
         class_name: :User
 
+    # include SecureRandom
+
+    def self.random_code
+        possible_url = SecureRandom.urlsafe_base64
+        until !ShortenedURL.exists(short_url: possible_url)
+            possible_url = SecureRandom.urlsafe_base64
 end
